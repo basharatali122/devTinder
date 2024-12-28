a80996ac741677ab9941bf2b166c3a7a8093cf7c
basharatali112 <basharat62200150@gmail.com>
2024-12-28 22:53:58 +0500

    playing with advanced routes rigix params

--- src/app.js
+++ src/app.js
@@ -3,37 +3,32 @@ const express = require("express");
 const app = express();
 
 
-//request handler function
-app.get("/user",(req,res)=>{
-    res.send("this is user get request to the server")
+app.get("/ab*c",(req,res)=>{
+    console.log("active")
+    res.send("this is ab*c routes")
 })
-app.post("/user",(req,res)=>{
-    res.send("this is post request to the server")
+app.get("/ab?cd",(req,res)=>{
+    res.send("this is ab?cd routes")
 })
-app.delete("/user",(req,res)=>{
-    res.send("user is deleted")
-})
-app.put("/user",(req,res)=>{
-    res.send("request is updated")
-})
-app.use("/work/work2",(req,res)=>{
-    res.send("this is work 2")
+app.get("/ab+cd",(req,res)=>{
+    res.send("this is ab+cd routes")
 })
 
-app.use("/work",(req,res)=>{
-    res.send("welcome to the work ")
+app.get("/ab(cd)?ef",(req,res)=>{
+    res.send("this is ab(cd)?ef routes")
+})
+app.get(/a/,(req,res)=>{
+    res.send("this is the regix part of routes")
+})
+app.get(/.*fly$/,(req,res)=>{
+    res.send(
+        "this will search everthing which end with the fly."
+    )
+
 })
 
-app.use("/morning",(req,res)=>{
-    res.send("happy good morning")
-})
-app.use("/special",(req,res)=>{
-    res.send("you are very special for me")
-})
-app.use((req,res)=>{
-    res.send("hello from the server")
-})
 app.listen(3000,()=>{
     console.log("server is listning on 3000 port")
 });
+
 
