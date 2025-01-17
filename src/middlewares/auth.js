137db6bd407c258abec9af3ae243d386faad9af2
basharatali112 <basharat62200150@gmail.com>
2025-01-18 00:11:23 +0500

    handle routes if user is not login

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,7 +4,7 @@ const userAuth = async (req, res, next) => {
  try{
     const {token} = req.cookies;
  if(!token){
-    throw new Error("Invalid token");
+    return res.status(401).send("please login!..");
  }
 const decodeData = await jwt.verify(token,"DEV@Tinder790")
 const {_id} = decodeData;
