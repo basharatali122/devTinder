af252b4474251d0b049939756d3299244572caaa
basharatali112 <basharat62200150@gmail.com>
2025-01-25 13:06:26 +0500

    add signup form + bug fixed

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,13 +1,9 @@
-const express = require("express")
+const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const authRouter = express.Router();
 const { validateSignUpdata } = require("../utils/validation");
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
-
-
-
-
 
 authRouter.post("/signup", async (req, res) => {
   try {
@@ -29,45 +25,53 @@ authRouter.post("/signup", async (req, res) => {
       emailId,
       skills,
     });
-    await user.save();
-    res.send("user added to the database successfuly");
+    const savedUser = await user.save();
+    const token = await user.getJWT();
+
+    // console.log(token);
+
+    // add the token to the cookie and then send the respons to the user
+    res.cookie("token", token, { expires: new Date(Date.now() + 900000) });
+    res.json({
+      message: "user added to the database successfuly",
+      data: savedUser,
+    });
   } catch (err) {
     res.status(400).send("Error:" + err.message);
   }
 });
 
 authRouter.post("/login", async (req, res) => {
-    try {
-      const { password, emailId } = req.body;
-  
-      const user = await User.findOne({ emailId: emailId });
-      if (!user) {
-        throw new Error("invalid credentials");
-      }
-  
-      const isPasswordValid = await user.validatePassword(password);
-      if (isPasswordValid) {
-        //create jwt token
-        const token = await user.getJWT();
-  
-        // console.log(token);
-  
-        // add the token to the cookie and then send the respons to the user
-        res.cookie("token", token, { expires: new Date(Date.now() + 900000)});
-  
-        res.send(user);
-      } else {
-        throw new Error("invalid credentials");
-      }
-    } catch (err) {
-      res.status(400).send("Error:" + err.message);
-    }
-  });
-  
-  authRouter.post('/signout', async (req,res)=>{
-    res.cookie("token",null,{expires: new Date(Date.now())})
+  try {
+    const { password, emailId } = req.body;
 
-    res.send("logout successfull..")
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("invalid credentials");
+    }
+
+    const isPasswordValid = await user.validatePassword(password);
+    if (isPasswordValid) {
+      //create jwt token
+      const token = await user.getJWT();
+
+      // console.log(token);
+
+      // add the token to the cookie and then send the respons to the user
+      res.cookie("token", token, { expires: new Date(Date.now() + 900000) });
+
+      res.send(user);
+    } else {
+      throw new Error("invalid credentials");
+    }
+  } catch (err) {
+    res.status(400).send("Error:" + err.message);
   }
-  );
-module.exports=authRouter;
+});
+
+authRouter.post("/signout", async (req, res) => {
+  res.cookie("token", null, { expires: new Date(Date.now()) });
+
+  res.send("logout successfull..");
+});
+module.exports = authRouter;
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require ("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const requestAuth = express.Router();
